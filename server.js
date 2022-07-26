6f1f163b7c968ad8a67207956cde774f608881d5
Yaron L <ylapidot@gmail.com>
2022-07-26 15:38:57 +0300

    Another test

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http=require('http');
 const app=require('./app');
 const server=http.createServer(app);
-const port=process.env.port || 3000
+const port=process.env.PORT || 3000
 server.listen(port,()=>{console.log('server started')}); 
