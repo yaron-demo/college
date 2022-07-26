92de7ccf77ccdaf89f17b126d924dfec401a1bf1
Yaron Lapidot <ylapidot@gmail.com>
2022-07-26 12:25:43 +0300

    Update AAAAp Js

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express=require('express');
 const app=express();
 require('dotenv').config();
 const cors=require('cors');
+const bodyParser = require('body-parser');
+const { urlencoded } = require('body-parser');
 
 
 
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended:true}));
 
 app.get('*',(req,res)=>{
 
